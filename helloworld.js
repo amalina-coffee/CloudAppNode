c288b81c1df9d37ed7c160663d83e45da6ed3e70
amalina-coffee <55571716+amalina-coffee@users.noreply.github.com>
2020-12-08 17:34:17 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,10 @@
 var http = require('http');
+var userCount = 0;
 http.createServer(function (req, res) {
+  userCount++;
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
+  res.write('Hello World!\n');
+  res.write('We have had ' + userCount + visitors!\n');
   res.end();
 }).listen(8080);
 
